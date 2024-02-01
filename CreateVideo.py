d3b6f684b7c1bcee41f1124624763506c89509f5
o-s1 <d3v.exgaming@gmail.com>
2024-02-01 17:37:27 -0500

    Update CreateVideo.py

--- CreateVideo.py
+++ CreateVideo.py
@@ -18,17 +18,28 @@ count = len(images)
 frame = cv2.imread(images[0])
 height, width, channels = frame.shape
 size = (width, height)
-out = cv2.VideoWriter('sunset.mp4', cv2.VideoWriter_fourcc(*'DIVX'), 5, size)
+
+print("Please enter either sunset or sunrise")
+print("or 1 for sunset and 2 for sunrise")
+video_type = input(": ")
+
+if video_type == "sunset" or video_type == "sunset" or video_type == "1" or video_type == "2":
+    print("")
+else:
+    print("Wrong input")
+    exit(0)
+
+out = cv2.VideoWriter('sunset.mp4' if video_type.lower() == "sunset" or "1" else 'sunrise.mp4', cv2.VideoWriter_fourcc(*'DIVX'), 5, size)
 
 # sunrise
-# for i in range(count-1, 0, -1):
-#     frame = cv2.imread(images[i])
-#     out.write(frame)
-
-# sunset
-for i in range(0, count-1):
-    frame = cv2.imread(images[i])
-    out.write(frame)
+if video_type.lower() == "sunset" or "1":
+    for i in range(count-1, 0, -1):
+        frame = cv2.imread(images[i])
+        out.write(frame)
+else:
+    for i in range(0, count-1):
+        frame = cv2.imread(images[i])
+        out.write(frame)
 
 out.release()
 print("Done")
